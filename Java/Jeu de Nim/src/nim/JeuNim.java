be7145148d9f9590262ba75cf9b3aa6e385cea7d
Aisuki <corentin25besnier@gmail.com>
2022-02-09 11:53:36 +0100

    Update projet

--- Java/Jeu de Nim/src/nim/JeuNim.java
+++ Java/Jeu de Nim/src/nim/JeuNim.java
@@ -4,8 +4,6 @@ import controler.Controleur;
 import modele.modele;
 import vue.vue;
 
-// Ici, on importe nos trois packages et on code notre fonction main, qui permet de lancer le jeu.
-
 public class JeuNim {
     public static void main(String[] args) throws CloneNotSupportedException {
         vue.Ihm ihm = new vue.Ihm();
@@ -13,6 +11,6 @@ public class JeuNim {
         constructeurJeu.construireJeu();
         modele.Tas lesTas = constructeurJeu.getPlateau();
         Controleur.ControleurJeu controleurJeu = new Controleur.ControleurJeu(ihm, lesTas);
-        controleurJeu.CommencerJeu();
+        controleurJeu.commencerJeu();
     }
 }
